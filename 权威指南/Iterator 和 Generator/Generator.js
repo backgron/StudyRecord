01658a53cd1bb5a7f3e1cb470b2fd83181ae27c9
backgron <backgron@163.com>
2022-03-04 18:43:28 +0800

    http相关

--- 权威指南/Iterator 和 Generator/Generator.js
+++ 权威指南/Iterator 和 Generator/Generator.js
@@ -1,41 +1,72 @@
-`
-  Genertor 是 ES6 提供的一种异步编程解决方案
-  Generator 是一个状态机 ，可以返回一个遍历器对象(Iterator)
-  可以把 Generator 赋值给 Symbol.iterator 属性
-  Gererator 中的this 不知想Generator函数  可以使用 f = F.call(F.prototype)  
-`
+// `
+//   Genertor 是 ES6 提供的一种异步编程解决方案
+//   Generator 是一个状态机 ，可以返回一个遍历器对象(Iterator)
+//   可以把 Generator 赋值给 Symbol.iterator 属性
+//   Gererator 中的this 不知想Generator函数  可以使用 f = F.call(F.prototype)  
+// `
 
-//基本使用
-function* hellowWorldGenerator() {
-  yield 'hello'
-  yield 'world'
-  return 'ending'
+// //基本使用
+// function* hellowWorldGenerator() {
+//   yield 'hello'
+//   yield 'world'
+//   return 'ending'
+// }
+
+// let hw = hellowWorldGenerator()
+// let res = hw.next()
+// while (!res.done) {
+//   console.log(res.value) // hello  world
+//   res = hw.next()
+// }
+// console.log(res.value) // ending
+
+
+// // next 方法可以传一个参数 会被当做上一个 yield 表达式的返回值，yield 表达式本身没有返回值
+// // 可以用来在每个 yield 阶段向方法中注入值
+
+// function* foo(x) {
+//   let y = 2 * (yield(x + 1))
+//   let z = yield(y / 3)
+//   return (x + y + z)
+// }
+
+// var a = foo(5)
+// console.log(a.next()); //{ value: 6, done: false }
+// console.log(a.next()); //{ value: NaN, done: false }
+// console.log(a.next()); //{ value: NaN, done: true }
+
+// var a = foo(5)
+// console.log(a.next()); //{ value: 6, done: false }
+// console.log(a.next(12)); //{ value: 8, done: false }
+// console.log(a.next(13)); //{ value: 42, done: true }
+
+
+function getDate(time) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res('这是你的date' + time)
+    }, 1000)
+  })
 }
 
-let hw = hellowWorldGenerator()
-let res = hw.next()
-while (!res.done) {
-  console.log(res.value) // hello  world
-  res = hw.next()
-}
-console.log(res.value) // ending
-
-
-// next 方法可以传一个参数 会被当做上一个 yield 表达式的返回值，yield 表达式本身没有返回值
-// 可以用来在每个 yield 阶段向方法中注入值
-
-function* foo(x) {
-  let y = 2 * (yield(x + 1))
-  let z = yield(y / 3)
-  return (x + y + z)
+let asyncFn = function* () {
+  let date1 = yield getDate(1)
+  console.log(date1)
+  let date2 = yield getDate(2)
+  console.log(date2)
 }
 
-var a = foo(5)
-console.log(a.next()); //{ value: 6, done: false }
-console.log(a.next()); //{ value: NaN, done: false }
-console.log(a.next()); //{ value: NaN, done: true }
+function run(asyncFn) {
+  let it = asyncFn()
 
-var a = foo(5)
-console.log(a.next()); //{ value: 6, done: false }
-console.log(a.next(12)); //{ value: 8, done: false }
-console.log(a.next(13)); //{ value: 42, done: true }
+  let go = (res) => {
+    if (res.done) return res.value
+    return res.value.then(date => {
+      return go(it.next(date))
+    })
+  }
+  go(it.next())
+
+}
+
+run(asyncFn)
--- 权威指南/this指向和call_bind_apply/call_bind_apply.js
+++ 权威指南/this指向和call_bind_apply/call_bind_apply.js
@@ -1,63 +1,90 @@
-// call
+// // call
+// Function.prototype.myCall = function (context, ...args) {
+//   if (typeof this !== 'function') {
+//     throw new Error('type error')
+//   }
+
+//   context = context || globalThis
+
+//   context.fn = this
+
+//   res = context.fn(...args)
+
+//   delete context.fn
+
+//   return res
+// }
+
+// // apply
+// Function.prototype.myApply = function (context, args) {
+//   //判断当前是否为函数
+//   if (typeof this !== 'function') {
+//     throw new Error('type error')
+//   }
+
+//   //确定context的值
+//   context = context || globalThis
+
+//   //将this方法挂载到context  准备通过传入对象调用
+//   context.fn = this
+
+//   //调用  传参
+//   res = context.fn(...args)
+
+//   //返回结果
+//   return res
+
+// }
+
+// //bind
+// Function.prototype.myBind = function (context, ...args) {
+//   if (typeof this !== 'function') {
+//     throw new Error('type error')
+//   }
+
+//   //绑定存this
+//   let fn = this
+
+//   //返回
+//   return function (...prop) {
+//     return fn.apply(context, args.concat(...prop))
+//   }
+// }
+
+// let a = {
+//   a: 1
+// }
+
+// function log(x, y) {
+//   return this.a + x + y
+// }
+
+// let fn = log.bind()
+// console.log(fn(2))
+
+
+
 Function.prototype.myCall = function (context, ...args) {
-  if (typeof this !== 'function') {
-    throw new Error('type error')
-  }
-
-  context = context || globalThis
-
-  context.fn = this
-
-  res = context.fn(...args)
-
-  delete context.fn
-
+  let ctx = context || window
+  ctx.fn = this
+  res = ctx.fn(...args)
+  Reflect.deleteProperty(ctx, 'fn')
   return res
 }
 
-// apply
-Function.prototype.myApply = function (context, args) {
-  //判断当前是否为函数
-  if (typeof this !== 'function') {
-    throw new Error('type error')
-  }
-
-  //确定context的值
-  context = context || globalThis
-
-  //将this方法挂载到context  准备通过传入对象调用
-  context.fn = this
-
-  //调用  传参
-  res = context.fn(...args)
-
-  //返回结果
-  return res
-
-}
-
-//bind
 Function.prototype.myBind = function (context, ...args) {
-  if (typeof this !== 'function') {
-    throw new Error('type error')
-  }
-
-  //绑定存this
   let fn = this
-
-  //返回
-  return function (...prop) {
-    return fn.apply(context, args.concat(...prop))
+  return function (...props) {
+    return fn.apply(context, args.concat(props))
   }
 }
 
-let a = {
-  a: 1
+function sayHi() {
+  console.log(this.name)
 }
 
-function log(x, y) {
-  return this.a + x + y
+let obj = {
+  name: 'zhansan'
 }
 
-let fn = log.bind()
-console.log(fn(2))
+sayHi.myCall(obj)
