d33eeffcac211ac041e0721492b8b2de3e5ecdab
backgron <1319536124@qq.com>
2022-06-16 09:31:10 +0800

    格式优化

--- JavaScript原理_ES6＋/数组扁平化&深浅拷贝/深浅拷贝.js
+++ JavaScript原理_ES6＋/数组扁平化&深浅拷贝/深浅拷贝.js
@@ -1,43 +1,38 @@
 let obj = {
   a: 1,
-  b: function () {
-
-  },
+  b: function () {},
   c: {
-    d: 2
-  }
+    d: 2,
+  },
 }
-
 
 //深拷贝
 
-// 1.JSON.parse(JSON.stringify()) 
+// 1.JSON.parse(JSON.stringify())
 // 只能拷贝符合json的对象，function\symbol等拷贝不了
-console.log(
-  JSON.parse(JSON.stringify(obj))
-)
+console.log(JSON.parse(JSON.stringify(obj)))
 
 // // 简单的深拷贝
 let deepClone = function (obj) {
   let newObj = {}
   //判断是不是object
-  if (typeof obj !== 'object') {
+  if (typeof obj !== "object") {
     return obj
   }
 
   //遍历对象中的所有属性
   for (let key in obj) {
     // 对象或者数组
-    if (typeof obj[key] === 'object') {
+    if (typeof obj[key] === "object") {
       //数组
       if (Array.isArray(obj[key])) {
         // 递归复制数组
-        newObj[key] = obj[key].map(item => deepClone(item)) //1  通过map复制数组
+        newObj[key] = obj[key].map((item) => deepClone(item)) //1  通过map复制数组
       } else {
         // 递归复制对象
         newObj[key] = deepClone(obj[key]) //2  通过递归复制对象
       }
-    } else if (typeof obj[key] === 'function') {
+    } else if (typeof obj[key] === "function") {
       //返回新方法
       newObj[key] = obj[key].bind(newObj) //3  通过bind复制方法
     } else {
@@ -48,11 +43,7 @@ let deepClone = function (obj) {
   return obj
 }
 
-console.log(deepClone(obj));
-
-
-
-
+console.log(deepClone(obj))
 
 // 浅拷贝
 
@@ -65,7 +56,7 @@ console.log(deepClone(obj));
 // });
 
 // //  Object.getPrototypeOf()   原型链  所有属性(包括原型链上的)
-// // Object.getOwnPropertyDescriptors()      
+// // Object.getOwnPropertyDescriptors()
 // //   所指定对象的所有自身属性的描述符，如果没有任何自身属性，则返回空对象。
 // console.log(
 //   Object.create(
